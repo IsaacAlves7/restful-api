d7b7afb7a10a89fd5198bede392bec620b00dc80
IsaacAlves7 <isaacalves0720@gmail.com>
2022-09-07 13:07:38 -0300

    1st commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use('/', require('./route/postsRoute'));
+app.use(function (error, req, res, next) {
+	if (error.message === 'Post already exists') {
+		return res.status(409).send(e.message);
+	}
+	if (error.message === 'Post not found') {
+		return res.status(404).send(e.message);
+	}
+	res.status(500).send(error.message);
+});
+
+app.get('/', (req,res) => {
+    res.redirect('/posts');
+});
+
+app.listen(3000);
